ba2079d81d36ddbe3597419c0819ff383929886c
jaumealonso21 <jaumealonso@hotmail.com>
2017-05-17 19:57:43 +0200

    versió 3.5

---
+++ public_html/js/amigos.js
@@ -0,0 +1,19 @@
+
+
+var logo;
+
+window.onload = ini();
+
+function ini(){
+   var logo = document.getElementById("logotip");
+    alert(logo);
+//    logo.addEventListener("click", function(){
+//        window.location.href = '../index.html';
+//    });
+}
+
+function inici() {
+    window.location.href = '../index.html';
+}
+
+
