aea9bdd100c39d35b0b1a78d3332742a8b580c43
jaumealonso21 <jaumealonso@hotmail.com>
2017-05-24 17:18:07 +0200

    versió Final

--- public_html/js/amigos.js
+++ public_html/js/amigos.js
@@ -1,17 +1,4 @@
 
-
-var logo;
-
-window.onload = ini();
-
-function ini(){
-   var logo = document.getElementById("logotip");
-    alert(logo);
-//    logo.addEventListener("click", function(){
-//        window.location.href = '../index.html';
-//    });
-}
-
 function inici() {
     window.location.href = '../index.html';
 }
